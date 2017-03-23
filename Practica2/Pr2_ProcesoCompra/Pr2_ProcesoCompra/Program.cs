767fa13bdfbee05d4ef941f0c19e8c5ae24b420c
Fran Maciá <fjme2@alu.ua.es>
2017-03-23 09:48:52 +0100

    Arreglado cliente

--- Practica2/Pr2_ProcesoCompra/Pr2_ProcesoCompra/Program.cs
+++ Practica2/Pr2_ProcesoCompra/Pr2_ProcesoCompra/Program.cs
@@ -16,7 +16,7 @@ namespace Pr2_ProcesoCompra
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ProcesoCompra());
         }
     }
 }
